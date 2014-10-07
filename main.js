1c956247037363efc3bb94375b4f7a0bbddc0c72
Brad Brooks <brad@graficabythesea.com>
2014-10-07 12:13:01 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,269 @@
+define(function(require, exports, module) {
+	var fileInfo = {};
+
+	function addIcon(extension, icon, color, size) {
+		fileInfo[extension] = {
+			icon: icon,
+			color: color,
+			size: size
+		};
+	}
+	function addAlias(extension, other) {
+		fileInfo[extension] = fileInfo[other];
+	}
+	function getDefaultIcon(extension) {
+		if (extension === '') {
+			return {
+				color: '#94a3a7',
+				icon: '\uf011'
+			};
+		}
+
+		var hue = 0;
+		var saturnation = 90;
+		var lightness = 50;
+
+		for (var i = 0; i < extension.length; ++i) {
+			hue += extension.charCodeAt(i) * 42 * (i + 2);
+			hue %= 256;
+			saturnation = (saturnation + (extension.charCodeAt(i) % 30) + 70) / 2;
+			lightness = (lightness + (extension.charCodeAt(i) * 3 % 40) + 30) / 2;
+		}
+
+		return {
+			color: 'hsl(' + Math.round(hue) + ', ' + Math.round(saturnation) + '%, ' + Math.round(lightness) + '%)',
+			icon: '\uf12f'
+		};
+	}
+
+	// XML
+	addIcon('xml',    '\uf05f', '#ff6600');
+	addIcon('html',   '\uf13b', '#d28445');
+	addAlias('htm', 'html');
+
+	// Stylesheets
+	addIcon('css',    '\uf13c', '#6a9fb5');
+	addIcon('scss',   '\uf13c', '#c6538c');
+	addAlias('sass',  'scss');
+	addIcon('less',   '\uf13c', '#3b6bb2');
+	addIcon('styl',   '\uf13c', '#b3d107');
+
+	// JavaScript
+	addIcon('js',     '\ue097', '#f4bf75');
+	addIcon('ejs',    '\uf05f', '#f4bf75');
+	addIcon('ts',     '\uf05f', '#0074c1');
+    addIcon('coffee', '\ue0b3', '#c9905e');
+    addIcon('json',   '\uf096', '#F4BF75');
+    addIcon('ls',     '\uf269', '#369bd7');
+
+	// Server side
+	addIcon('php',    '\ue09a', '#6976c3');
+    addIcon('sql',    '\uf096', '#c67f07');
+
+	// Java
+	addIcon('java',   '\ue098', '#75b4de');
+    addAlias('class', 'java');
+
+	// Ruby
+	addIcon('rb',     '\uf199', '#9b111e');
+	addAlias('erb',   'rb');
+	addAlias('rdoc',  'rb');
+	addIcon('feature','\uf11a', '#4e8b39');
+
+	// Shell and friends
+	addIcon('sh',     '\ue0b7');
+    addIcon('bat',    '\ue0b7');
+    addIcon('command','\ue0b7');
+
+	// Templating
+	addIcon('jade',   '\uf13b', '#01dfa5');
+
+	// Images
+	addIcon('png',    '\uf012', '#dbb1a9');
+    addIcon('jpg',    '\uf012', '#dedfa3');
+    addAlias('jpeg',  'jpg');
+    addIcon('tiff',   '\uf012', '#ff4000');
+    addIcon('ico',    '\uf012', '#b6d2d1');
+    addIcon('svg',    '\uf012', '#c0c5eb');
+
+	addIcon('gif',    '\uf012', '#aaecc0');
+
+	// Videos
+	addIcon('mp4',    '\uf094');
+    addAlias('webm',  'mp4');
+    addAlias('ogg',   'mp4');
+
+	// Audio
+	addIcon('mp3',    '\uf094');
+    addAlias('wav',   'mp3');
+
+	// Fonts
+	addIcon('ttf',    '\uf094', '#b42950');
+	addIcon('eot',    '\uf094', '#b36908');
+	addIcon('woff',   '\uf094', '#7f4bb2');
+
+	// Readme and Markdown
+	addIcon('md', '\uf0c9', '#c36b35');
+    addAlias('markdown', 'md');
+
+	// Git
+	addIcon('gitignore', '\uf084', '#a0422e', 18);
+    addIcon('gitmodules', '\uf020');
+    addIcon('gitattributes', '\uf020');
+
+	// Webservers
+	addIcon('htaccess', '\uf02f');
+    addIcon('htpasswd', '\uf02f');
+    addIcon('conf',   '\uf02f');
+
+	// Archive
+	addIcon('zip',    '\uf013', '#008858');
+	addIcon('rar',    '\uf013', '#005888');
+	addIcon('7z',     '\uf013', '#880058');
+	addIcon('tgz',    '\uf013', '#7900bc');
+	addIcon('tar',    '\uf013', '#885800');
+	addIcon('gz',     '\uf013', '#588800');
+	addIcon('bzip',   '\uf013', '#884300');
+
+	// Settings
+	addIcon('project', '\uf013');
+    addAlias('jscsrc', 'project');
+    addAlias('jshintrc', 'project');
+    addAlias('csslintrc', 'project');
+    addAlias('todo', 'project');
+    addAlias('classpath', 'project');
+
+	// Other text files
+	addIcon('txt',    '\uf011');
+    addIcon('log',    '\uf011');
+    addIcon('npmignore', '\uf084', '#a0422e', 18);
+    addIcon('yml',   '\uf011');
+    addIcon('ls', '\uf011');
+    addIcon('org', '\uf011');
+
+	var ProjectManager = brackets.getModule('project/ProjectManager');
+	var DocumentManager = brackets.getModule('document/DocumentManager');
+	var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+
+	var MainViewManager;
+	var FileTreeView;
+	try {
+		MainViewManager = brackets.getModule('view/MainViewManager');
+		FileTreeView = brackets.getModule("project/FileTreeView");
+	} catch (e) {
+
+	}
+
+	ExtensionUtils.loadStyleSheet(module, "styles/style.css");
+
+	function renderFiles() {
+		$('#project-files-container ul').removeClass('jstree-no-icons').addClass('jstree-icons');
+
+		var $items = $('#project-files-container li>a');
+
+		$items.each(function(index) {
+			var ext = ($(this).find('.extension').text() || $(this).text().substr(1) || '').substr(1).toLowerCase();
+			var lastIndex = ext.lastIndexOf('.');
+			if (lastIndex > 0) {
+				ext = ext.substr(lastIndex + 1);
+			}
+
+			var data;
+
+			if ($(this).parent().hasClass('jstree-leaf')) {
+				data = fileInfo.hasOwnProperty(ext) ? fileInfo[ext] : getDefaultIcon(ext);
+			} else {
+				return;
+			}
+
+			var $new = $(this).find('.jstree-icon');
+			$new.text(data.icon);
+			$new.addClass('file-icon');
+			$new.css({
+				color: data.color,
+				fontSize: (data.size || 16) + 'px'
+			});
+		});
+	}
+	function renderWorkingSet() {
+		$('.open-files-container li>a>.file-icon, #open-files-container li>a>.file-icon').remove();
+
+		var $items = $('.open-files-container li>a, #open-files-container li>a');
+
+		$items.each(function(index) {
+			var ext = ($(this).find('.extension').text() || $(this).text() || '').substr(1).toLowerCase();
+			var lastIndex = ext.lastIndexOf('.');
+			if (lastIndex > 0) {
+				ext = ext.substr(lastIndex + 1);
+			}
+
+			var data = fileInfo.hasOwnProperty(ext) ? fileInfo[ext] : getDefaultIcon(ext);
+
+			var $new = $('<div>');
+			$new.text(data.icon);
+			$new.addClass('file-icon');
+			$new.css({
+				color: data.color,
+				fontSize: (data.size || 16) + 'px'
+			});
+			$(this).prepend($new);
+		});
+	}
+
+	function projectOpen() {
+		var events = 'load_node.jstree create_node.jstree set_text.jstree';
+
+		renderFiles();
+
+		$('#project-files-container').off(events, renderFiles);
+		$('#project-files-container').on(events, renderFiles);
+	}
+
+	if (!FileTreeView) {
+		// Old Brackets, FileTreeView.addIconProvider not supported
+		$(ProjectManager).on('projectOpen projectRefresh', projectOpen);
+		projectOpen();
+	} else {
+		// New Brackets, FileTreeView.addIconProvider supported
+		FileTreeView.addIconProvider(function(entry) {
+			if (!entry.isFile) {
+				return;
+			}
+
+			var ext = entry.name;
+			var lastIndex = ext.lastIndexOf('.');
+			if (lastIndex >= 0) {
+				ext = ext.substr(lastIndex + 1);
+			} else {
+				ext = '';
+			}
+
+			var data = fileInfo.hasOwnProperty(ext) ? fileInfo[ext] : getDefaultIcon(ext);
+
+			var $new = $('<ins>');
+			$new.text(data.icon);
+			$new.addClass('file-icon file-tree-view-icon');
+			$new.css({
+				color: data.color,
+				fontSize: (data.size || 16) + 'px'
+			});
+			return $new;
+		});
+	}
+
+
+	if (MainViewManager) {
+		$(MainViewManager).on("workingSetAdd workingSetAddList workingSetRemove workingSetRemoveList workingSetSort workingSetUpdate workingSetMove activePaneChange currentFileChange paneLayoutChange paneCreate paneDestroy", function() {
+			//renderWorkingSet();
+			setTimeout(renderWorkingSet, 1);
+			// TODO: call renderWorkingSet directly. See issue #30
+		});
+	} else {
+		$(DocumentManager).on("workingSetAdd workingSetAddList workingSetRemove workingSetRemoveList fileNameChange pathDeleted workingSetSort", function() {
+			renderWorkingSet();
+		});
+	}
+
+
+	renderWorkingSet();
+});
---
+++ readme.md
@@ -0,0 +1,53 @@
+Grafica Brackets Icons
+==============
+This extension adds file icons to the brackets file tree, and it works with brackets' new react-based file tree. I can take no credit for this plugin as it's based solely on the work of [ivogabe](https://github.com/ivogabe) and [drewbkoch](https://github.com/drewbkoch). Basically I took the code from Ivo's original, and changed out the icons he used for the icons that Drew used. Check out the original repositories at: [Brackets-Icons](https://github.com/ivogabe/Brackets-Icons) and [Brackets-File-Icons](https://github.com/drewbkoch/Brackets-File-Icons)
+
+Supported files
+---------------
+The following files are supported at the moment:
+
+ - JavaScript
+ - JSON
+ - TypeScript
+ - CoffeeScript
+ - LiveScript
+ - HTML, HTM
+ - SVG
+ - XML
+ - PHP, SQL
+ - Java
+ - CSS, SASS, Less, Stylus
+ - Shell script, Batch
+ - Jade
+ - TXT
+ - Log
+ - Markdown
+ - PNG, JPG, JPEG, TIFF, ICO
+ - GIF
+ - MP4, WebM, OGG
+ - MP3, WAV
+ - EOT, TTF, WOFF
+ - GitIgnore, GitModules
+ - NPMIgnore
+ - HTAccess, HTPasswd, Conf
+ - YAML
+ - Project, Jscsrc, Jshintrc, Csslintrc, Todo, Classpath
+ - Zip, Rar, 7z, Tgz, Tar, Gz, Bzip
+ - EJS
+ - COMMAND
+ - GitAttributes
+ - ORG (EditorConfig.org)
+
+How to install
+--------------
+Download the zip file from this page, Open Brackets, and click the extensions button on the right. Drop the downloaded zip file on the extensions window at bottom left. That's it.
+
+License
+-------
+Grafica Brackets Icons is licensed under the [MIT license](http://opensource.org/licenses/MIT). The original Brackets Icons and Brackets File Icons are also licensed under the MIT license.
+
+Font Awesome icons by Dave Gandy - http://fontawesome.io
+
+Octicons icons by Github - http://github.com
+
+Pictonic icons by Pictonic - http://pictonic.co
